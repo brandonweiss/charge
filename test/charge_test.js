77d279c15119326eacb7cd7c22d3adc7206a93e8
Brandon Weiss <brandon@anti-pattern.com>
2018-03-12 21:16:00 -0700

    Add providing data files to the JSX templates

--- test/charge_test.js
+++ test/charge_test.js
@@ -1,12 +1,26 @@
 import test from "ava"
 import dedent from "dedent"
 import fs from "node-fs-extra"
+import glob from "glob"
+import path from "path"
 import charge from "../lib/charge"
 import { createFiles, assertFiles } from "./helpers/filesystem"
 
 let tmpPathPrefix = "tmp/tests"
 
 test.afterEach.always(t => {
+  // This bizarre incantation is necessary because after a test JSX file has been created
+  // it is eventually required using `require`, which caches it, so even after the file
+  // has been deleted, when a file with the same name is created in a subsequent test the
+  // next `require` will return the cached version unless we delete it from the cache first.
+  let files = glob.sync(`${tmpPathPrefix}/**/*.{js,jsx}`, {
+    nodir: true,
+  })
+
+  files.forEach((file) => {
+    delete require.cache[path.resolve(file)]
+  })
+
   fs.removeSync(tmpPathPrefix)
 })
 
@@ -91,5 +105,40 @@ test("renders a JSX template as HTML", (t) => {
 
   assertFiles(t, targetDirectory, {
     "index.html": "<html></html>",
+  })
+})
+
+test("loads data from data files and passes it to the JSX template", (t) => {
+  let sourceDirectory = `${tmpPathPrefix}/source`
+  let targetDirectory = `${tmpPathPrefix}/target`
+
+  createFiles(sourceDirectory, {
+    data: {
+      "stuff.json": dedent`
+        {
+          "foo": "bar"
+        }
+      `
+    },
+    "index.html.jsx": dedent`
+      const React = require("react")
+
+      module.exports = class extends React.Component {
+
+        render() {
+          return <p>{this.props.data.stuff.foo}</p>
+        }
+
+      }
+    `
+  })
+
+  charge.build({
+    source: sourceDirectory,
+    target: targetDirectory,
+  })
+
+  assertFiles(t, targetDirectory, {
+    "index.html": "<p>bar</p>",
   })
 })
