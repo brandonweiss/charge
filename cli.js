68895f92bfbed46fb236465d543b9e7a99b8dd06
Brandon Weiss <brandon@anti-pattern.com>
2018-06-28 17:52:09 -0700

    Add a CLI and build command

---
+++ cli.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const meow = require("meow")
+const charge = require("./lib/charge")
+
+const cli = meow(`
+  Usage
+    ❯ charge build <source directory> <target directory>
+`)
+
+let command = cli.input[0]
+
+switch (command) {
+  case undefined:
+    return cli.showHelp()
+  case "build":
+    let source = cli.input[1]
+    let target = cli.input[2]
+
+    return charge.build({ source, target })
+}
