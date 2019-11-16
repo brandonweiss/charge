f6966f506fa1dc0a34f35796646b76cb11a2ed8e
Brandon Weiss <brandon@anti-pattern.com>
2019-11-15 20:47:51 -0800

    Fix tests on Windows
    The problem is that the snapshots of the filesystem are generated on *nix (with forward slashes) so when the tests are run on Windows (which uses back slashes) the tests fail.
    
    What’s not clear is why this worked before and then suddenly started failing.  The commits around when it started failing don’t seem related in any way 🤷🏼‍♂️

--- test/helpers/filesystem.js
+++ test/helpers/filesystem.js
@@ -71,12 +71,28 @@ const snapshotDirectoryStructure = (t) => {
     nodir: true,
   })
 
+  files = files.map((file) => {
+    if (pathSeparator === "\\") {
+      return file.replace(new RegExp(`\\${pathSeparator}`, "g"), "/")
+    }
+
+    return file
+  })
+
   t.snapshot(files)
 }
 
 const snapshotFileContents = (t) => {
   let files = globSyncNormalize(`${targetDirectory}/**/*`, {
     nodir: true,
+  })
+
+  files = files.map((file) => {
+    if (pathSeparator === "\\") {
+      return file.replace(new RegExp(`\\${pathSeparator}`, "g"), "/")
+    }
+
+    return file
   })
 
   let targetFilesystem = files.reduce((filesystem, file) => {
