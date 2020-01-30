d109f6c74ba89ffe6eec8a4cd541182d6ca7471d
Brandon Weiss <brandon@anti-pattern.com>
2020-01-30 15:44:59 -0800

    Switch from Gauges to Fathom

--- docs/components/tracking.html.jsx
+++ docs/components/tracking.html.jsx
@@ -2,18 +2,17 @@ export default ({ environment }) => {
   let isProduction = environment === "production"
 
   let scriptBody = `
-    var _gauges = _gauges || [];
-    (function() {
-      var t   = document.createElement('script');
-      t.type  = 'text/javascript';
-      t.async = true;
-      t.id    = 'gauges-tracker';
-      t.setAttribute('data-site-id', '5c96822e173f68216ada4ae8');
-      t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');
-      t.src = 'https://d2fuc4clr7gvcn.cloudfront.net/track.js';
-      var s = document.getElementsByTagName('script')[0];
-      s.parentNode.insertBefore(t, s);
-    })();
+    (function(f, a, t, h, o, m){
+    a[h]=a[h]||function(){
+    (a[h].q=a[h].q||[]).push(arguments)
+    };
+    o=f.createElement('script'),
+    m=f.getElementsByTagName('script')[0];
+    o.async=1; o.src=t; o.id='fathom-script';
+    m.parentNode.insertBefore(o,m)
+    })(document, window, 'https://cdn.usefathom.com/tracker.js', 'fathom');
+    fathom('set', 'siteId', 'FOEDUFMD');
+    fathom('trackPageview');
   `
 
   let script = {
